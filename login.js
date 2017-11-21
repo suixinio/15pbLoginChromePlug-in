49f371a7e24694fe03b95c8d6f3f98ee1e4d7710
suixin <suixinio@163.com>
2017-11-21 12:51:11 +0800

    15PB-login
    create project

---
+++ login.js
@@ -0,0 +1,6 @@
+document.getElementById("login-user").value="***";
+document.getElementById("login-pwd").value="***";
+if(document.getElementById("log-button").value == "登录")
+{
+	document.getElementById("log-button").click()		
+}
